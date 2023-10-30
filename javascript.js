f236c48d1b5eb4911769aaf491c6c2fd205eac75
joshMozie <moziejoshua@gmail.com>
2023-10-30 15:38:13 +0100

    javascript

---
+++ javascript.js
@@ -0,0 +1,59 @@
+console.log("Hello World");
+const a = 823;
+const b = 7482;
+const c = 54156;
+const d = 516;
+const e = 546;
+const f = 98;
+const g = 7491;
+const h = 3541;
+const i = 8423;
+const j = 1561;
+
+const sumOfAll = a + b + c + d + e + f + g + h + i + j;
+
+const frequency = 10;
+
+const average = sumOfAll / frequency;
+
+console.log(average);
+
+const varianceA = average - a;
+
+console.log(varianceA);
+
+const varianceB = average - b;
+
+console.log(varianceB);
+
+const varianceC = average - c;
+
+console.log(varianceC);
+
+const varianceD = average - d;
+
+console.log(varianceD);
+
+const varianceE = average - e;
+
+console.log(varianceE);
+
+const varianceF = average - f;
+
+console.log(varianceF);
+
+const varianceG = average - g;
+
+console.log(varianceG);
+
+const varianceH = average - h;
+
+console.log(varianceH);
+
+const varianceI = average - i;
+
+console.log(varianceI);
+
+const varianceJ = average - j;
+
+console.log(varianceJ);
